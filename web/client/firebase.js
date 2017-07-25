d9ecbba620ade20af51c5bebaaf8cffdd5b67035
amylynncheng <amylynncheng@gmail.com>
2017-07-25 15:21:52 -0500

    second commit

--- web/client/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyCpRi72bcIaqoyKT83LHxVx53DBODPVm0M",
-    authDomain: "healthycoping-f6c2f.firebaseapp.com",
-    databaseURL: "https://healthycoping-f6c2f.firebaseio.com",
-    projectId: "healthycoping-f6c2f",
-    storageBucket: "healthycoping-f6c2f.appspot.com",
-    messagingSenderId: "516378072561"
-  };
- firebase.initializeApp(config);
